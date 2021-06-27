1ef84b489dd6767a12cb84b40aa56aed54c02c44
Leonardo J. Caballero G <leonardocaballero@gmail.com>
2021-06-27 19:09:18 -0400

    Added python-dotenv support

--- README.md
+++ README.md
@@ -1,1 +1,44 @@
-#bot-python
+# bot-python
+
+An Instagram Bot for publish comments random to post.
+
+## Download
+
+$ git clone https://github.com/rafaelnepomuceno00/bot-python.git
+
+
+## Install
+
+For install you need install Geckodriver and the Python libraries:
+
+### Geckodriver
+
+You need install the Geckodriver, with following command:
+
+$ wget https://github.com/mozilla/geckodriver/releases/download/v0.28.0/geckodriver-v0.28.0-linux64.tar.gz
+$ sudo sh -c 'tar -x geckodriver -zf geckodriver-v0.28.0-linux64.tar.gz -O > /usr/bin/geckodriver'
+$ sudo chmod +x /usr/bin/geckodriver
+$ rm geckodriver-v0.28.0-linux64.tar.gz
+
+
+### Python libraries
+
+You need install the Python libraries, with following command:
+
+$ cd bot-python
+$ pip3 install -r requeriments.txt
+
+### Instagram Bot settings
+
+You need to set the Instagram Bot settings into .env file, with following command:
+
+$ cp .env.example .env
+$ nano .env
+
+
+## Run Bot
+
+For run the Python Bot, with following command:
+
+$ python3 ./igBot.py
+
--- igBot.py
+++ igBot.py
@@ -1,25 +1,40 @@
-from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
+import os
 import time
 import random
+from dotenv import load_dotenv
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+load_dotenv()
+
+USER_IG = os.getenv('USER_IG')
+PASSWORD_IG = os.getenv('PASSWORD_IG')
+DRIVER_PATH = os.getenv('DRIVER_PATH')
+LINK_DO_POST = os.getenv('LINK_DO_POST')
 
 
 class InstagramBot:
-    def __init__(self, userName, password):
+    def __init__(self, userName, password, link_do_post, driver):
+        """ Class constructor """
         self.userName = userName
         self.password = password
-        self.driver = webdriver.Firefox(
-            executable_path='/home/rafael/Documentos/geckodriver-v0.28.0-linux64/geckodriver')
+        self.link_do_post = link_do_post
+        self.driver = webdriver.Firefox(executable_path=driver)
 
     def login(self):
+        """ Instagram login function """
+
         driver = self.driver
+        # The link for instagram login
         driver.get(
             "https://www.instagram.com/accounts/login/?source=auth_switcher")
         time.sleep(4)
+        # The username field
         userfield = driver.find_element_by_xpath('//input[@name="username"]')
         userfield.click()
         userfield.clear()
         userfield.send_keys(self.userName)
+        # The password field
         pswdfield = driver.find_element_by_xpath("//input[@name='password']")
         pswdfield.click()
         pswdfield.clear()
@@ -29,29 +44,40 @@ class InstagramBot:
 
     @staticmethod
     def typing(phrase, wheretype):
+        """ Function for typing the comment to post """
         for letters in phrase:
             wheretype.send_keys(letters)
             time.sleep(random.randint(1, 5)/30)
 
     def coment(self):
+        """ Function of comments to post """
         driver = self.driver
-        driver.get("link do post")
+        # The link to do post
+        driver.get(self.link_do_post)
         time.sleep(4)
         driver.execute_script('window.scrollTo(0,300);')
 
         for i in range(0, 1000):
             try:
-                coments = ['Quero ganhar!', 'Contando com a sorte', 'Qualquer Coisa', 'Comentando','Vou ganhar', 'Já ganhei!', 'Quero', 'Espero muito ganhar', 'Tentando', 'Não custa nada tentar',
-                           'Quero muito!', 'Espero ganhar!', '$$$', '$','$$','$$$', '$$$$$', '$$$$', '$$$$$$$$$$$$$' '$$$$$$' '$$$$$$$']
+                # The comments random to post
+                coments = [
+                    'Quero ganhar!', 'Contando com a sorte', 'Qualquer Coisa', 'Comentando',
+                    'Vou ganhar', 'Já ganhei!', 'Quero', 'Espero muito ganhar', 'Tentando',
+                    'Não custa nada tentar', 'Quero muito!', 'Espero ganhar!', '$$$', '$', '$$',
+                    '$$$', '$$$$$', '$$$$', '$$$$$$$$$$$$$' '$$$$$$' '$$$$$$$'
+                ]
                 driver.find_element_by_class_name("Ypffh").click()
+                # The coment field
                 comentfield = driver.find_element_by_class_name("Ypffh")
                 time.sleep(random.randint(4, 10))
                 self.typing(random.choice(coments), comentfield)
                 time.sleep(random.randint(15, 40))
+                # The Post button
                 driver.find_element_by_xpath(
                     "//button[contains(text(), 'Post')]").click()
                 time.sleep(random.randint(4, 10))
-                driver.get("link do post")
+                # The link to do post
+                driver.get(self.link_do_post)
                 time.sleep(random.randint(4, 10))
                 driver.execute_script('window.scrollTo(0,300);')
 
@@ -63,7 +89,8 @@ class InstagramBot:
                 time.sleep(2)
 
 
-bot = InstagramBot('user', 'senha')
+bot = InstagramBot(USER_IG, PASSWORD_IG, LINK_DO_POST, DRIVER_PATH)
 bot.login()
 time.sleep(5)
 bot.coment()
+
